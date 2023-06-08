27d760facafd67157c8eb64e778c5cbdba619149
mohd <mohammed.mahfodh@gmail.com>
2023-06-08 11:31:16 +0300

    added routes ,contoroller, layout.ejs

---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+exports.index_get = (req, res) => {
+    res.render('home/index')
+}
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const IndexController = require('../controllers/index')
+
+router.get('/',IndexController.index_get)
+
+module.exports = router
