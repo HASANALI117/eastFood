008624ad90818fd5f48251832f3460d43f328546
zainabjalil <zainab.jh@hotmail.com>
2023-06-08 17:10:30 +0300

    updated

---
+++ views/drinks/drinksDev.js
@@ -0,0 +1,8 @@
+<a href="">
+           <div class="box b-3">
+               <div class="in-cards">
+                      <div>
+                         <h3>Drinks</h3>
+                      </div>
+               </div>
+          </div></a>
---
+++ views/main/mainDev.js
@@ -0,0 +1,8 @@
+<a href="">
+<div class="box b-2">
+    <div class="in-cards">
+        <div>
+           <h3>Main Dishes</h3>
+       </div>
+   </div>
+</div></a>
