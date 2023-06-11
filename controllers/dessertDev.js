2b9d64febd77ebfc099fbe5d9211b19847ed4f3f
zainabjalil <zainab.jh@hotmail.com>
2023-06-11 09:57:42 +0300

    ayshay

--- controllers/dessertDev.js
+++ controllers/dessertDev.js
@@ -1,9 +1,4 @@
 
-exports.dessertDev_get=async(req,res)=>{
-    try{
-        res.render('desserts/dessertDev',{dessertDev})
-    }catch(error){
-        console.log(error.message)
-        res.send(error.message)
-    }
+exports.dessertDev_get=(req,res)=>{
+
 }
